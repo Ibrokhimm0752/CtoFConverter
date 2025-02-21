26b4910ecbec14ec71ce93d6af0c99eb7ffd0a9a
Ibrokhimm0752 <Ibrokhimm0752@hstat.org>
2025-02-21 18:46:08 -0500

    updated

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
-
 public class CtoFConverter {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double celsius = 0;
         String trash;
         boolean done = false;
-
         do {
             System.out.print("Enter temperature in Celsius: ");
             if (in.hasNextDouble()) {
                 celsius = in.nextDouble();
-                in.nextLine(); // Clear the buffer
+                in.nextLine();
                 done = true;
             } else {
                 trash = in.nextLine();
@@ -19,10 +17,8 @@ public class CtoFConverter {
                 System.out.println("You have to enter a valid number!");
             }
         } while (!done);
-
         double fahrenheit = (celsius * 9/5) + 32;
         System.out.printf("Equivalent temperature in Fahrenheit: %.2f°F\n", fahrenheit);
-
         in.close();
     }
 }
